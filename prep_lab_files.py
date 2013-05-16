25bade250952e043a0703cbe2470fd054614f1e7
yasemin <yasemin.boluk@gmail.com>
2013-05-16 17:01:59 -0400

    create dictionary from lab files
    includes pronunciation

--- prep_lab_files.py
+++ prep_lab_files.py
@@ -10,6 +10,8 @@ from os import makedirs
 from os.path import exists
 from subprocess import call
 
+# extracts text from a textgrid file given the file name, file encoding, and the tier
+# on which the text is found
 def extract_text(filename, utf8, tiernum):
 
 	if utf8 == False:
@@ -41,7 +43,9 @@ def extract_text(filename, utf8, tiernum):
 		text = tmp.group(1)
 	
 	return text
-	
+
+# creates a .lab file with the text from the textgrid file given the original file
+# and the line of text
 def create_file(file, textline):
 	
 	newfilename = file.replace(".TextGrid", ".lab")
@@ -51,7 +55,46 @@ def create_file(file, textline):
 	
 	return newfile
 
-#test to create lab file first
+# extracts individual words from lab list and puts into list
+def extract_word(file, list):
+
+	f = codecs.open(file, "r", "utf-8")
+	string = f.readline()
+	
+	newlist = string.rsplit(" ")
+	
+	for word in newlist:
+		list.append(word)
+		
+	f.close()
+
+	return list
+
+# eliminates duplicates, taken from get_german_dict.py code
+def no_copies(data):
+	"""Eliminates identical entries in a list."""
+	data_new = []; prev = None
+	for line in data:
+		if prev is not None and not line == prev:
+			data_new.append(prev)
+		prev = line
+	return data_new
+
+
+# on same line as each word, add its pronunciation
+def add_pronunciation(words):
+
+	new_words = []
+
+	for item in words:
+		list_string = list(item)
+		for character in list_string:
+			item = item + " " + character
+		new_words.append(item)
+
+	return new_words
+
+
 
 #form
 print"""
@@ -114,6 +157,7 @@ makedirs(filedir + olddir)
 # make a list of the files
 file_list = glob(filedir+"*")
 
+# for each file, make a .lab file and save the textgrid to a separate folder
 for file in file_list:
 	if ".TextGrid" in file:
 	
@@ -126,4 +170,48 @@ for file in file_list:
 		#create a new file with line of text
 		newfile = create_file(file, textline)
 		
+# make a dictionary using the lab files just created
+
+# updated list of files
+lab_list = glob(filedir+"*")
+
+dictionary_list = []
+
+# for each file, get words from file and 
+for file in lab_list:
+	if ".lab" in file:
+		
+		#extract each word in file, put into dictionary list
+		dictionary_list = extract_word(file, dictionary_list)
+		
+# sort list
+sorted_words = sorted(dictionary_list)
+		
+# remove duplicates
+unique_words = no_copies(sorted_words)
+
+# make pronunciations
+words_pronounced = add_pronunciation(unique_words)
+		
+# put list into a dictionary text file	
+dictionary_file = codecs.open(filedir + "/dictionary.txt", 'w', 'utf-8')
+
+for word in words_pronounced:
+	dictionary_file.write(word)
+	dictionary_file.write("\n")
+
+dictionary_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
